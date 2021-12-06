327632b4a4961f56b5aeca2dd162800a6162ec64
Per Sandström <per.j.sandstrom@gmail.com>
2021-12-06 21:01:01 +0100

    2021 day 6

---
+++ 2021/06/1.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+input = [row.strip() for row in sys.stdin if row.strip() != '']
+data = [int(nr) for nr in input[0].split(',')]
+ages = [0]*9
+
+for i in data:
+    ages[i] = ages[i] + 1
+
+for day in range(80):
+    to_give_birth = ages[0]
+    for i in range(1, len(ages)):
+        ages[i-1] = ages[i]
+    ages[6] = ages[6] + to_give_birth
+    ages[8] = to_give_birth
+
+print(sum(ages))

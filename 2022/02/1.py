ce4fe204667701c0465dc864d46cfab58cc62596
Per Sandström <per.j.sandstrom@gmail.com>
2022-12-02 10:09:05 +0100

    2022 day 2

---
+++ 2022/02/1.py
@@ -0,0 +1,22 @@
+import sys
+
+rounds = [row.strip() for row in sys.stdin]
+
+points = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6}
+
+score = 0
+
+for round in rounds:
+    score += points[round] 
+
+print(score)
+
---
+++ 2022/02/2.py
@@ -0,0 +1,22 @@
+import sys
+
+rounds = [row.strip() for row in sys.stdin]
+
+points = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7}
+
+score = 0
+
+for round in rounds:
+    score += points[round] 
+
+print(score)
+

c2ded1a08ff1e2a3b3d043cbc227d15d7a60ba5e
Per Sandström <per.j.sandstrom@gmail.com>
2022-12-08 20:52:13 +0100

    2022 day 8

--- 2022/08/1.py
+++ 2022/08/1.py
@@ -5,13 +5,13 @@ w, h = len(input_data), len(input_data[0])
 trees = [int(i) for i in "".join(input_data)]
 
 visible = 0
-for i, hight in enumerate(trees):
+for i, height in enumerate(trees):
     x, y = i%w, i//h
     visible += (
         x==0 or y==0 or x==w-1 or y==h-1 or 
-        max(trees[h*(i//w):i]) < hight or
-        max(trees[h*(i//w)+x+1:(y+1)*w]) < hight or
-        max(trees[x:i-1:w]) < hight or
-        max(trees[h*(y+1)+x::w]) < hight)
+        max(trees[h*(i//w):i]) < height or
+        max(trees[h*(i//w)+x+1:(y+1)*w]) < height or
+        max(trees[x:i-1:w]) < height or
+        max(trees[h*(y+1)+x::w]) < height)
 
 print(visible)

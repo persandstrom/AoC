f87250f1f4a73ee87a983a6e9caea528955355eb
Per Sandström <per.j.sandstrom@gmail.com>
2023-12-18 22:21:47 +0100

    2023 day 17

---
+++ 2023/day_17_part_01_test.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "day_17_part_01.h"
+#include "file_reader_test_double.h"
+
+#include <string>
+
+// Test suite for the FabricPiece class
+class Fixture_17_01 : public ::testing::Test {};
+
+TEST_F(Fixture_17_01, test_input_1) {
+    // Test the FromString function with valid input
+    const std::vector<std::string> test_input = {
+        "2413432311323",
+        "3215453535623",
+        "3255245654254",
+        "3446585845452",
+        "4546657867536",
+        "1438598798454",
+        "4457876987766",
+        "3637877979653",
+        "4654967986887",
+        "4564679986453",
+        "1224686865563",
+        "2546548887735",
+        "4322674655533",
+    };
+    FileReaderTestDouble reader(test_input);
+
+    EXPECT_EQ(day_17_part_01::run(reader), "102");
+}
---
+++ 2023/day_17_part_02.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <string>
+#include "file_reader.h"
+#include <vector>
+#include "aoc_util.h"
+#include "unordered_map"
+#include <queue>
+
+using namespace std;
+
+namespace day_17_part_02 {
+
+
+struct Crucible {
+    int x;
+    int y;
+    char direction;
+    int steps;
+    int heatloss;
+};
+
+struct Intersection {
+    array<int, 10> north{INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX};
+    array<int, 10> east{INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX};
+    array<int, 10> south{INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX};
+    array<int, 10> west{INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX};
+};
+
+using Intersections = unordered_map<string, Intersection>;
+
+auto cmp = [](const Crucible & c1, const Crucible & c2){return c1.heatloss > c2.heatloss;};
+using Crucibles = priority_queue<Crucible, vector<Crucible>, decltype(cmp)>; 
+
+void push_crucible(Intersections & intersections, Crucibles & crucibles, const Crucible & crucible ) {
+    auto id = aoc::coord_to_string(crucible.x, crucible.y);
+    if (intersections.count(id) == 0) {
+        intersections[id] = Intersection();
+    }
+    if(crucible.direction=='n' && crucible.heatloss < intersections.at(id).north[crucible.steps-1]) {
+        intersections.at(id).north[crucible.steps-1] = crucible.heatloss;
+        crucibles.push(crucible);
+    }
+    if(crucible.direction=='w' && crucible.heatloss < intersections.at(id).west[crucible.steps-1]) {
+        intersections.at(id).west[crucible.steps-1] = crucible.heatloss;
+        crucibles.push(crucible);
+    }
+    if(crucible.direction=='s' && crucible.heatloss < intersections.at(id).south[crucible.steps-1]) {
+        intersections.at(id).south[crucible.steps-1] = crucible.heatloss;
+        crucibles.push(crucible);
+    }
+    if(crucible.direction=='e' && crucible.heatloss < intersections.at(id).east[crucible.steps-1]) {
+        intersections.at(id).east[crucible.steps-1] = crucible.heatloss;
+        crucibles.push(crucible);
+    }
+};
+
+std::string run(FileReader& reader) {
+    auto map = reader.to_vector();
+    int height = map.size();
+    int width = map.at(0).size();
+    Intersections intersections;
+
+    Crucibles crucibles(cmp); 
+    crucibles.push(Crucible{0,1,'s',1,0});
+    crucibles.push(Crucible{1,0,'e',1,0});
+        
+    int best = 6*(width+height);
+
+    while (!crucibles.empty())
+    {
+        Crucible crucible = crucibles.top();
+        crucibles.pop();
+        crucible.heatloss += map[crucible.y][crucible.x] - '0';
+        if(crucible.heatloss >= best) continue;
+
+        if(crucible.x == width-1 && crucible.y== height-1 && crucible.heatloss < best) {
+            best = crucible.heatloss;
+        }
+
+        if(crucible.direction == 's') {
+            if(crucible.steps > 3 && crucible.x>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x-1, crucible.y, 'w', 1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.x<width-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x+1, crucible.y, 'e', 1, crucible.heatloss});
+            }
+            if(crucible.steps<10 && crucible.y<height-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y+1, 's', crucible.steps+1, crucible.heatloss});
+            }
+        }
+        if(crucible.direction == 'n') {
+            if(crucible.steps<10 && crucible.y>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y-1, 'n', crucible.steps+1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.x>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x-1, crucible.y, 'w', 1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.x<width-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x+1, crucible.y, 'e', 1, crucible.heatloss});
+            }
+        }
+        if(crucible.direction == 'e') {
+            if(crucible.steps > 3 && crucible.y>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y-1, 'n', 1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.y<height-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y+1, 's', 1, crucible.heatloss});
+            }
+            if(crucible.steps<10 && crucible.x<width-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x+1, crucible.y, 'e', crucible.steps+1, crucible.heatloss});
+            }
+        }
+        if(crucible.direction == 'w') {
+            if(crucible.steps<10 && crucible.x>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x-1, crucible.y, 'w', crucible.steps+1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.y>0) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y-1, 'n', 1, crucible.heatloss});
+            }
+            if(crucible.steps > 3 && crucible.y<height-1) {
+                push_crucible(intersections, crucibles, Crucible{crucible.x, crucible.y+1, 's', 1, crucible.heatloss});
+            }
+        } 
+    }
+    
+
+    
+        
+    return std::to_string(best);
+}
+}
